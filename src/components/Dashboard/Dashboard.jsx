845a81aa7f99af943399b65d345e9fa6768eb857
Thiago Rodrigues <thiago.rds@outlook.com.br>
2022-09-25 15:09:40 -0400

    feat: implementation of the form component, applying styles for desktop and mobile and capturing data from inputs

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,11 +2,13 @@
 import './Dashboard.css'
 // components
 import Header from "./Header/Header";
+import Form from './Form/Form';
 
 function Dashboard({setIsLoggedIn}) {
   return (
     <div className='containerDashboard'>
-      <Header setIsLoggedIn={setIsLoggedIn}/>
+          <Header setIsLoggedIn={setIsLoggedIn} />
+          <Form/>
     </div>
   );
 }
---
+++ src/components/Dashboard/Form/Form.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./Form.css";
+
+function Form() {
+
+  const [inputValue, setInputValue] = useState('')
+  const [typeOfValue, setTypeOfValue] = useState('moneyDeposit')
+
+
+
+
+  return (
+    <form onSubmit={(event)=> event.preventDefault()}  className="form flex f-column gap-2">
+      <section className="section__description flex f-column">
+        <label htmlFor="description">Descrição</label>
+        <input className="input-df" type="text" name="description"  id="description" placeholder=" Digite aqui sua descrição"/>
+        <span>Ex: Compras de roupas</span>
+      </section>
+
+      <section className="flex f-justify-between f-align-center gap-1">
+
+        <div className="div__value flex f-column">
+          <label htmlFor="value">Valor</label>
+          <input className="input-df" type="number" name="value" id="value" placeholder="1" value={inputValue} onChange={(event)=> setInputValue(event.target.value)}/>
+        </div>
+
+        <div className="div_typeOfValue flex f-column">
+          <label htmlFor="typeOfValue">Tipo de Valor</label>
+          <select className="input-df" id="typeOfValue" value={typeOfValue} onChange={(event)=> setTypeOfValue(event.target.value)}>
+            <option value="moneyDeposit">Entrada</option>
+            <option value="moneyOut">Saída</option>
+          </select>
+        </div>
+      </section>
+
+
+      <button className="btn btn__main" type="submit">
+        Inserir Valor
+      </button>
+    </form>
+
+  );
+}
+
+export default Form;
